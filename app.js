0298e74bae39e69e5bf378e269074a890010ed5b
Syed-Nazeer9 <Syednazeer679@gmail.com>
2023-02-22 02:00:39 +0500

    first

---
+++ app.js
@@ -0,0 +1,416 @@
+//Alerts
+
+//Q #1
+//alert("hello world");
+
+//Q#2
+//alert("Error! Please enter a valid password");
+
+//Q#3
+//alert("Welcome to js land...\n Happy coding");
+
+//Q#4
+//alert("Welcome to js Land...");
+//alert("Happy Coding! \n Prevent this page from creating additional dialogs.");
+
+//Q#5
+// console.log(JavaScript Alert);
+// console.log(Hello... I can run JS through my web browser console );
+// console.log(fullName) 
+
+//Q#6
+
+
+//Chapter 2
+
+//VARIABLES FOR STRINGS
+
+//Q#1
+//var username;
+
+//Q#2
+//var myName ="Syed Muhammed Nazeer Ahmed"
+
+//Q#4
+//var studentName ="Syed Nazeer";
+//var age ="29";
+//var profile ="Certified Mobile Application Development";
+//alert(studentName);
+//alert(age);
+//alert(profile);
+
+//Q#5
+
+//var abc ="PIZZA \nPIZZ \nPIZ \nPI \nP";
+//alert(abc);
+
+//Q#6
+
+//var a ="Syednazeer";
+//var b ="679";
+//var c ="@gmail.com";
+//var email = a+b+c;
+//alert(email);
+
+//Q#7
+ //var book ="A smarter way to learn JavaScript";
+ //alert(book);
+
+
+//Q#8
+
+//var  browser ="Yah! I can write HTML content through JavaScript";
+//document.write( browser);
+
+//Q#9
+
+//var border ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//alert(border);
+
+//Chapter#3
+//VARIABLES FOR NUMBERS
+
+//Q#1
+//var age ="29";
+//alert(age);
+
+//Q#2
+//var visit = 14;
+//alert ("You have visited this site"+ visit + "times");
+
+//Q#3
+//var birthYear ="My birth year is 1993";
+//document.write( birthYear);
+
+//Q#4
+//var a ="Syed Nazeer";
+//var b =" ordered 5 T-shirt(s)";
+//var c =" Outfitter store";
+//var clothing =a+b+c;
+//document.write(clothing);
+
+//Chapter#4
+//VARIABLE NAMES: LEGAL & ILLEGAL
+
+//Q#1
+
+//var a,b,c;
+
+//Q#2
+
+//Legal
+//var age;
+//var $abc;
+//var _xyz;
+//var name;
+//var email;
+ 
+//illegal
+//var @age;
+//var !abc;
+//var %xyz;
+//var 1name;
+//var *email;
+
+//Q#3
+
+//a
+//document.write("A heading stating “Rules for naming JS variables” ");
+//document.write("<br>");
+
+//b
+//document.write("Variable names can only contain $, letter, _, digit");
+//document.write("For example $my_1stVariable ");
+//document.write("<br>");
+
+//c
+//document.write("Variables must begin with a $, _,latter  ");
+//document.write("<br>");
+
+//d
+//document.write("Variable names are case sensitive ");
+//document.write("<br>");
+
+//e
+//document.write(" Variable names should not be JS Keywords");
+
+//MATH EXPRESSIONS
+
+//Q#1
+
+//var n1 = 3;
+//var n2 = 5;
+//sum = n1+n2;
+//document.write ("sum of " + n1 +" and " + n2 + " is " + sum);
+
+//Q#2
+//var n1 = 3;
+//var n2 = 5;
+//Sub = n1-n2;
+//document.write ("Subtraction of " + n1 +" and " + n2 + " is " + Sub);
+
+//var n1 = 3;
+//var n2 = 5;
+//Mul = n1*n2;
+//document.write ("Mul of " + n1 +" and " + n2 + " is " + Mul);
+
+//var n1 = 3;
+//var n2 = 5;
+//Div = n1/n2;
+//document.write ("Div of " + n1 +" and " + n2 + " is " + Div);
+
+//var n1 = 3;
+//var n2 = 5;
+//Mod = n1%n2;
+//document.write ("Mod of " + n1 +" and " + n2 + " is " + Mod);
+
+//Q#3
+
+//a. Declare a variable.
+//var abc;
+ 
+//b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
+//document.write (abc);
+
+//c. Initialize the variable with some number.
+//abc = 5;
+//document.write( "Initial value " + abc);
+
+//e. Increment the variable.
+//abc++;
+
+//f. Show the value of variable in your browser like “Value after increment is: 5”.
+//document.write ("Value after increment is: " + abc);
+
+//g. Add 7 to the variable.
+//document.write (7 +abc);
+
+//h. Show the value of variable in your browser like “Value after addition is: 13”.
+
+//abc += 7;
+//document.write("Value of addition is:" + abc);
+
+//i. Decrement the variable.
+//abc--;
+
+//j. Show the value of variable in your browser like “Value after decrement is: 12”.
+//document.write("value after Decrement is :" +abc--);
+
+//k. Show the remainder after dividing the variable’s value by 3. 
+ //abc%=3;
+ 
+ //document.write("The remainder after dividing the variable’s value by 3: "+abc);
+
+//Q#4
+//var ticketCost= 600;
+//document.write("Total cost to buy 5 tickets to a movier is "+ticketCost*5+" PKR");
+
+//Q#5
+
+// var table = 8;
+// document.write("Table of "+table+ "<br>");
+// document.write(table+" * 1 = "+table*1+"<br>");
+// document.write(table+" * 2 = "+table*2+"<br>");
+// document.write(table+" * 3 = "+table*3+"<br>");
+// document.write(table+" * 4 = "+table*4+"<br>");
+// document.write(table+" * 5 = "+table*5+"<br>");
+// document.write(table+" * 6 = "+table*6+"<br>");
+// document.write(table+" * 7 = "+table*7+"<br>");
+// document.write(table+" * 8 = "+table*8+"<br>");
+// document.write(table+" * 9 = "+table*9+"<br>");
+// document.write(table+" * 10 = "+table*10+"<br>");
+
+//Q#6
+
+
+
+
+
+
+
+//Q#7
+// document.write("<h2>Shopping cart</h2> <br>");
+// var item1 =650;
+// document.write("price of item1 is "+(item1)+"<br>" );
+// var Quantity =3;
+// document.write("Quantity of item1 is 3 <br>");
+// var cost1 =item1*Quantity;
+// var item2 =100;
+// document.write("price of item2 is "+(item2)+"<br>" );
+// var Quantity =7;
+// document.write("Quantity of item2 is 7 <br>");
+// var cost2 =item2*Quantity;
+// var charges= 100;
+// document.write("Shipping Charges 100 <br> <br>");
+// document.write("Total cost of your orderis "+(cost1+cost2+charges));
+
+
+
+//Q#8
+// document.write(" <h2> Marks Sheet </h2> <br> <br>");
+// var Total_marks =980;
+// document.write("Total marks:"+Total_marks+"<br>");
+// var obtained =804;
+// document.write("Marks obtained:"+obtained+"<br>")
+// var Precentage =obtained/Total_marks*100;
+// document.write("percentage "+ Precentage);
+
+
+//Q#9
+// document.write("<h2> Currency in PKR </h2> <br> <br>");  
+// var us_dollars =261.68;
+// var us_convert =us_dollars*10;
+// document.write(us_convert+"<br>");
+// var Saudi_Riyals =69.76;
+// var Saudi_convert=Saudi_Riyals*25;
+// document.write(Saudi_convert+"<br>");
+// document.write("Total Currency in PKR: "+(us_convert+Saudi_convert));
+
+//Q#10
+
+// var value1 =10;
+// var operation =value1+5 *10 /2;
+// document.write(operation);
+
+//Q#11
+// document.write("<h2>Age Calculator</h2> <br> <br>");
+// var Current_Year =2023;
+// document.write("Current Year:"+Current_Year+"<br>");
+// var Birth_Year =1993;
+// document.write("Birth Year:"+Birth_Year+"<br>");
+// var Your_Age =Current_Year-Birth_Year;
+// document.write("Your Age is:"+Your_Age);
+
+//Q#12
+
+// document.write("<h2>The Geometrize</h2>");
+// var radius = 20;
+// const PI = 3.142;
+// var c_of_circle = 2 * PI * radius;
+// var area_of_circle = PI * (radius * radius);
+// document.write("The radius of circle is:" + radius +"<br>" +"The Circucmerence is : "+ c_of_circle + "<br>");
+// document.write("Area of Circle is : " + area_of_circle);
+
+//Q#13
+
+// document.write("<h2>The Lifetime Supply Calculator </h2> <br>.");
+// var favourite_snack ="chocolate chip";
+// document.write ( "favourite snack: "+favourite_snack+"<br>" );
+// var current_age = 30;
+// document.write("current age: "+current_age+ "<br>" );
+// var E_M = 65;
+// document.write("Estimated aximum Age:"+ E_M +"<br>");
+// var A_S = 3;
+// document.write ("Amount of snacks per day:"+ A_S +"<br>");
+
+
+
+
+
+//Chapter # 06 To 09
+
+//Q#01
+
+// document.write ("Result: <br>")
+// var a = 10;
+// document.write ("The  value of a is: "+ a +"<br>");
+// document.write("................................. <br>   <br>");
+// document.write("The value of ++a is: "+ ++a + "<br>");
+// document.write ("Now the value of a is: "+ a +"<br> <br> ");
+// document.write("The value of a++ is: "+ a++ +"<br>");
+// document.write ("Now the value of a is: "+ a +"<br> <br> ");
+// document.write("The value of --a is: "+ --a +"<br>");
+// document.write ("Now the value of a is: "+ a +"<br> <br> ");
+// document.write("The value of a-- is: "+ a-- +"<br>");
+// document.write ("Now the value of a is: "+ a +"<br> ");
+
+//Q#2
+
+// var a = 2 , b = 1;
+// var result = --a - --b + ++b + b-- ;
+// document.write (--a +"<br>");
+// document.write ( --a - --b +"<br>");
+// document.write ( --a - --b + ++b +"<br>");
+// document.write ( --a - --b + ++b + b-- +"<br>");
+// document.write ( "The value of a is "+ a +"<br>");
+// document.write ( "The value of b is "+ b +"<br>");
+// document.write ( "The value of resul is "+ result);
+
+//Q#3
+
+// var userName = prompt("Hello! Input your name:");
+// alert ("The username is : "+ userName);
+
+//Q#05
+
+
+// var table = +prompt("Input your number ");
+// document.write("Table of "+table+ "<br>");
+// document.write(table+" * 1 = "+table*1+"<br>");
+// document.write(table+" * 2 = "+table*2+"<br>");
+// document.write(table+" * 3 = "+table*3+"<br>");
+// document.write(table+" * 4 = "+table*4+"<br>");
+// document.write(table+" * 5 = "+table*5+"<br>");
+// document.write(table+" * 6 = "+table*6+"<br>");
+// document.write(table+" * 7 = "+table*7+"<br>");
+// document.write(table+" * 8 = "+table*8+"<br>");
+// document.write(table+" * 9 = "+table*9+"<br>");
+// document.write(table+" * 10 = "+table*10+"<br>");
+
+//Q#6
+
+
+//USER INPUT & CONDITIONAL  STATEMENT
+
+
+//Q#1
+// var city = prompt("Enter The City Name");
+// if(city =="Karachi" || city == "karachi"){
+// document.write("Welcome to City of Lights");
+// }
+// else{
+//     document.write(":(");
+// } ;
+
+//Q#2
+// var gender = prompt("Enter Your Gender");
+// if( gender =="Male" || gender == "male"){
+//     document.write("Good Morning Sir");
+// }
+// else if(gender =="Female" || gender =="female"){
+//     document.write(" Good Morning Ma’am");
+// }
+// else {
+// document.write ("Not Exist");
+// }
+
+//Q#3
+// var road_trafic = prompt("input Color of road trafic");
+// if( road_trafic =="Red" || road_trafic == "red"){
+//     document.write("Must Stop");
+// }
+// else if (road_trafic =="Yellow" || road_trafic == "yellow"){
+//     document.write("Ready to move");
+// }
+// else if (road_trafic =="Green" || road_trafic == "green"){
+//     document.write("Move now");
+// }
+// else{
+//     document.write ("Color is Not Match From thr trafic Signal");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
